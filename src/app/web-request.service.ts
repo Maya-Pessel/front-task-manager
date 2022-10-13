7e4e2b536208f345b257edea9b7cd2e95ef280f3
maya.pessel <maya.pessel@gmail.com>
2022-10-13 10:10:26 +0200

    add heroku root

--- src/app/web-request.service.ts
+++ src/app/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'https://project-taskmanager.herokuapp.com/';
+    this.ROOT_URL = 'https://project-taskmanager.herokuapp.com';
   }
 
   get(uri: string) {
